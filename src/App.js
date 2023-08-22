711c850de607085dcfc94c73b79c83d76b953c84
llja <lesterlim.ja@gmail.com>
2023-08-23 00:41:48 +0800

    added input params

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React from "react";
 import logo from "./logo.png";
 import "./App.css";
+import axios from "axios";
 
-class App extends React.Component {
-  render() {
+const getCurrentWeather = async()=>{
+  try{
+    const response = await axios.get(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&limit=${limit}&appid=fe3a5aa180a67609e1cc3253d5b7f64c`
+
+    )
+  }
+}
+function App (){
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <input/>
         </header>
       </div>
     );
-  }
 }
 
 export default App;
