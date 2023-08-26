1c5d13f26ed1ccf6dd77a1fa1f38f1a51fda5acb
llja <lesterlim.ja@gmail.com>
2023-08-26 12:26:48 +0800

    try to call both api and quickly display the weather

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.png";
 import "./App.css";
 import axios from "axios";
 
-const getCurrentWeather = async()=>{
-  try{
-    const response = await axios.get(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&limit=${limit}&appid=fe3a5aa180a67609e1cc3253d5b7f64c`
 
-    )
-  }
-}
 function App (){
+
+  const [currWeather,setCurrWeather]= useState(null)
+
+  const getCurrentWeather = async(cityName='London',stateCode=0,countryCode=0,limit=5)=>{
+    try{
+      const response = await axios.get(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&limit=${limit}&appid=fe3a5aa180a67609e1cc3253d5b7f64c`
+      )
+      console.log(response)
+      const {lat,lon}=response.data[0];
+      console.log(lat,lon)
+      setCurrWeather()
+    }catch(err){
+      console.log(err);
+    };
+  };
+
+  useEffect(()=>{
+    getCurrentWeather();
+  },[]);
 
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <input/>
+          <h3>{currWeather}</h3>
         </header>
       </div>
     );
